7e447ae41fc4a6a76b4b1e7f1d2cf38fcdc2c39e
Treyvion Arte Johnson <tajohns22@gmail.com>
2023-12-05 22:10:30 -0600

    updated feed-item with styles

--- src/app/components/feed/feed-item.tsx
+++ src/app/components/feed/feed-item.tsx
@@ -1,24 +1,40 @@
+import { faComment, faShareFromSquare, faThumbsUp } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./feed-item.module.css";
 
 export default function NewsItem() {
 
     return (
-        <div className={`flex-dual`}>
+        <div className={`flex-dual ${styles["feed-item"]}`}>
             <div className={styles["feed-left"]}>
                 <img src="https://assets-prd.ignimgs.com/2023/11/29/mercenary-1-1701300675193.jpg" alt="News Image" />
             </div>
             <div className={styles["feed-right"]}>
-                <ul className={styles["feed-details"]}>
-                    <li>
+                <div className={styles["feed-details"]}>
+                    <div>
                         Source: <a href="ign.com">IGN</a>
-                    </li>
-                    <li>
+                    </div>
+                    <div>
                         June 4, 2023
-                    </li>
-                    <li>
+                    </div>
+                    <div className={"ellipsis-text"}>
                     In a livestream today developer Grinding Gear Games made a slew of announcements, including a first look at a new playable class in its upcoming ARPG Path of Exile 2: the mercenary. 
-                    </li>
-                </ul>
+                    </div>
+                    <div className={styles["feed-actions"]}>
+                        <div className={styles["feed-action-item"]}>
+                            <FontAwesomeIcon icon={faThumbsUp} />
+                            32
+                        </div>
+                        <div  className={styles["feed-action-item"]}>
+                            <FontAwesomeIcon icon={faComment} />
+                            124
+                        </div >
+                        <div className={styles["feed-action-item"]}>
+                            <FontAwesomeIcon icon={faShareFromSquare} />
+                            12
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
