dd6a89e019936d14db8de30ab2e3d6da35e50e6a
Treyvion Arte Johnson <tajohns22@gmail.com>
2024-06-10 22:05:24 -0500

    adding feed settings panel

---
+++ src/app/components/feed/feed-settings.tsx
@@ -0,0 +1,64 @@
+"use client";
+
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import { Accordion, AccordionDetails, AccordionSummary, Box, Drawer } from '@mui/material';
+import { useState } from 'react';
+import styles from "./feed-settings.module.css";
+
+const anchor = 'left';
+
+const FeedSettings = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleDrawer = (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+
+      setIsOpen(!isOpen);
+    };
+
+    return (
+        <div>
+            <button onClick={() => setIsOpen(!isOpen)} className={`${styles["btn-feed"]} btn-blg`}>
+                <SettingsIcon/>
+                Feed Settings
+            </button>
+            <Drawer
+                open={isOpen}
+                anchor={anchor}
+            >            
+                <div className={styles["feed-settings"]}>
+                    <h3 className={`h3-dark ${styles["feed-header"]}`}>
+                        <button className={`${styles["btn-drawer-toggle"]} btn-icon`} onClick={() => setIsOpen(!isOpen)}>
+                            <ArrowBackIosIcon/>
+                        </button>
+                        {" "}
+                        Feed Settings
+                    </h3>
+                    <Accordion defaultExpanded>
+                        <AccordionSummary
+                            expandIcon={<ExpandMoreIcon />}
+                            aria-controls="sites-content"
+                            id="sites-header"
+                        >
+                            News sources
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            
+                        </AccordionDetails>
+                    </Accordion>
+                </div>
+                
+            </Drawer>
+        </div>
+    )
+}
+
+export default FeedSettings;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,13 @@ import FeedList from './components/feed/feed-list';
 import Hero from './components/hero/hero';
 import styles from "./page.module.css";
 import DefaultError from './components/errors/defaultError';
+import FeedSection from './components/feed/feed-section';
 
 export default function Home() {
   return (
     	<>
         <Hero/>
-        <div className={styles['content-padding']}>    
-            <div className={styles["feed-container"]}>
-                <h1 className="h1-dark">Your Gaming Feed</h1>
-                <a href="/feed">
-                    View All
-                </a>
-                <ErrorBoundary FallbackComponent={DefaultError}>
-                    <FeedList />            
-                </ErrorBoundary>        
-            </div>
-        </div>
+        <FeedSection/>
       </>
     	
   )
