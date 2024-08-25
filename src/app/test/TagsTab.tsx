8823ef16c8655472a3541e158508260f8bbe9ab4
Treyvion Arte Johnson <tajohns22@gmail.com>
2024-08-25 14:56:33 -0500

    fixing single quote deploy issue

--- src/app/test/TagsTab.tsx
+++ src/app/test/TagsTab.tsx
@@ -4,7 +4,7 @@ export default function TagsTab() {
   return (
     <div>
       <div className="h2-bg">Followed Tags</div>
-      <p>Choose the tags you want to follow. We'll prioritize news stories related to these tags in your feed</p>
+      <p>Choose the tags you want to follow. We&quot;ll prioritize news stories related to these tags in your feed</p>
     </div>
   )
 }
