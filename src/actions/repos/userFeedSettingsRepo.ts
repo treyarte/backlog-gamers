ecbc87a7c3514550d54d70642ae6fcf94c37cbdb
Treyvion Arte Johnson <tajohns22@gmail.com>
2024-10-24 21:33:11 -0500

    working on user setting update

--- src/actions/repos/userFeedSettingsRepo.ts
+++ src/actions/repos/userFeedSettingsRepo.ts
@@ -1,2 +1,10 @@
+import { articleSitesEnum } from "@/app/models/enums/articleSitesEnum";
+import { prisma } from "@/libs/prisma";
 
-export async function updateEx
+export async function UpdateExcludeSource(userId:string, site:articleSitesEnum) {
+    
+    // prisma.user.
+    //     where: {id:userId},
+    //     data: {user}
+    // })
+}
